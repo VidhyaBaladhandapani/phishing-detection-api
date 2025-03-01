35f16c83be5bdd1696561b30463313c2a6d36972
VidhyaBaladhandapani <vidhyabaladhandapanii@gmail.com>
2025-03-01 15:28:25 +0530

    Initial commit

--- My Project/app.py
+++ My Project/app.py
@@ -49,6 +49,6 @@ def predict():
 
 import os
 
-if __name__ == '_main_':
+if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))  # Render uses a dynamic port
     app.run(host='0.0.0.0', port=port)
